39c4fc06225a1fc5381eea12f673b292d36c7760
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-10-12 13:48:06 -0300

    first commit

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index() {
+        $fornecedores = [
+            "Golden",
+            "Cordeiro",
+            "AVT",
+            "BelEnergy"
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
+    }
+}
---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,15 @@
+<h1>Principal</h1>
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Principal</a>
+    </li>
+
+    <li>
+        <a href="{{ route('site.sobre-nos')}}">Sobre Nós</a>
+    </li>
+
+    <li>
+        <a href=" {{ route('site.contato') }} ">Contato</a>
+    </li>
+</ul>
