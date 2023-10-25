57847df250b2e7fc2cb5ad81727c0a6a8deabab0
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-10-25 18:31:23 -0300

    $loop blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,30 +4,41 @@
    <h2>Não existem usuarios cadastrados</h2>
 @endif
 
-@if(isset($fornecedores))
+{{-- @if(isset($fornecedores))
    @for ($i = 0; isset($fornecedores[$i]); $i++)
       <div>
-      Fornecedor : {{$itemFornecedor[$i]['nome'] ?? 'sem nome'}}
-      <br>
-      status : {{$itemFornecedor[$i]['status'] ?? 'sem status'}}
-      <br> 
-      cnpj : {{$itemFornecedor[$i]['cnpj'] ?? 'sem cnpj'}}
-      <br>
-      <hr>
-   </div>
+         Fornecedor : {{$itemFornecedor[$i]['nome'] ?? 'sem nome'}}
+         <br>
+         status : {{$itemFornecedor[$i]['status'] ?? 'sem status'}}
+         <br> 
+         cnpj : {{$itemFornecedor[$i]['cnpj'] ?? 'sem cnpj'}}
+         <br>
+         <hr>
+      </div>
    @endfor
-@endif
+@endif --}}
 
-{{-- @foreach ($fornecedores as $itemFornecedor)
+@foreach ($fornecedores as $itemFornecedor)
+   Iteração atua: {{ $loop->iteration }} <br>
    <div>
+      @if ($loop->first)
+          Primeira iteração do loop
+      @endif
+      <br>
       Fornecedor : {{$itemFornecedor['nome'] ?? 'sem nome'}}
       <br>
       status : {{$itemFornecedor['status'] ?? 'sem status'}}
       <br> 
       cnpj : {{$itemFornecedor['cnpj'] ?? 'sem cnpj'}}
       <br>
+      @if ($loop->last)
+         Ultima iteração do loop
+         <br>
+         Total de registros {{ $loop->count }}
+      @endif
+      <hr>
    </div>
-@endforeach  --}}
+@endforeach
 
 @if (isset($fornecedores) && count($fornecedores) > 0  && count($fornecedores) < 10)
       <h2>Existem {{count($fornecedores)}} fornecedores cadastrados</h2> 
