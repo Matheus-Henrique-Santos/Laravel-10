2bf508ec6b73427a415941287898bce04572169b
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-10-25 17:48:30 -0300

    for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,7 +4,21 @@
    <h2>Não existem usuarios cadastrados</h2>
 @endif
 
-@foreach ($fornecedores as $itemFornecedor)
+@if(isset($fornecedores))
+   @for ($i = 0; isset($fornecedores[$i]); $i++)
+      <div>
+      Fornecedor : {{$itemFornecedor[$i]['nome'] ?? 'sem nome'}}
+      <br>
+      status : {{$itemFornecedor[$i]['status'] ?? 'sem status'}}
+      <br> 
+      cnpj : {{$itemFornecedor[$i]['cnpj'] ?? 'sem cnpj'}}
+      <br>
+      <hr>
+   </div>
+   @endfor
+@endif
+
+{{-- @foreach ($fornecedores as $itemFornecedor)
    <div>
       Fornecedor : {{$itemFornecedor['nome'] ?? 'sem nome'}}
       <br>
@@ -13,7 +27,7 @@
       cnpj : {{$itemFornecedor['cnpj'] ?? 'sem cnpj'}}
       <br>
    </div>
-@endforeach
+@endforeach  --}}
 
 @if (isset($fornecedores) && count($fornecedores) > 0  && count($fornecedores) < 10)
       <h2>Existem {{count($fornecedores)}} fornecedores cadastrados</h2> 
@@ -21,4 +35,5 @@
       <h2>Não existem fornecedores cadastrados</h2>
 @endif
 
-{{-- @unless (count($fornecedores) > 0  && count($fornecedores) < 10) @endunless //retorna apenas se for false --}} 
+
+{{-- @unless (count($fornecedores) > 0  && count($fornecedores) < 10) @endunless retorna apenas se for false --}} 
