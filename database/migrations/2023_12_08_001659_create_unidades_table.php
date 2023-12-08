7d9151cf2ce3298c5e8661684c4bfa2cdd0a0187
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-12-07 21:32:01 -0300

    create data-table unidades

---
+++ database/migrations/2023_12_08_001659_create_unidades_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
+            $table->timestamps();
+        });
+
+        Schema::table('product', function(Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreignId('unidade_id')->references('id')->on('unidades');
+        });
+
+        Schema::table('product_details', function(Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreignId('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_details', function(Blueprint $table) {
+            $table->dropForeign('product_details_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::table('product', function(Blueprint $table) {
+            $table->dropForeign('product_unidade_id_foreign');
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::dropIfExists('unidades');
+    }
+};
