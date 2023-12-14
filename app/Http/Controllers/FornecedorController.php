fe40c2a432375aeb47a48f9bbd76fc326d7c962b
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-12-13 21:57:41 -0300

    idetation

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index() {
-            $fornecedores = [
-                0 => ['nome' => 'apple', 'status' => 'Ativo', 'cnpj' => '12323412343'],
-                1 => ['nome' => 'xiaomi', 'status' => 'Ativo'],
-                2 => ['nome' => 'sansung', 'status' => 'Ativo'],
-                3 => ['nome' => 'hawei', 'status' => 'Ativo', 'cnpj' => '12323412343'],
-                4 => ['nome' => 'motorola', 'status' => 'Inativo', 'cnpj' => '12323412343'],          
-            ];
+    public function index()
+    {
+        $fornecedores = [
+            0 => ['nome' => 'apple', 'status' => 'Ativo', 'cnpj' => '12323412343'],
+            1 => ['nome' => 'xiaomi', 'status' => 'Ativo'],
+            2 => ['nome' => 'sansung', 'status' => 'Ativo'],
+            3 => ['nome' => 'hawei', 'status' => 'Ativo', 'cnpj' => '12323412343'],
+            4 => ['nome' => 'motorola', 'status' => 'Inativo', 'cnpj' => '12323412343'],
+        ];
 
         $usuarios = null;
 
