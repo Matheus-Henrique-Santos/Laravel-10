91142dcc430f0b4751d3b8d5c4917489cd9582d7
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2024-05-28 20:33:55 -0300

    test

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -10,7 +10,7 @@
         <div class="informacoes">
             <h1>Sistema Super Gestão MHS</h1>
             <p style="color:#0000ff; font-family:verdana; font-size:3px;">Sistema Super Gestão MHS</p>
-            <p>Software para gestão empresarial ideal para sua empresa..<p>
+            <p>Software para gestão empresarial ideal para sua empresa...<p>
             <div class="chamada">
                 <img src="/img/check.png">
                 <span class="texto-branco">Gestão completa e descomplicada</span>
